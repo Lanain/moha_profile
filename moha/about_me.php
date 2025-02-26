97fc082e4e70eb848d710ab9a7fdea974dfe8f07
AI90009 <200260303@aston.ac.uk>
2025-02-26 04:18:39 +0300

    updated profile

---
+++ moha/about_me.php
@@ -0,0 +1,124 @@
+<?php include("dependable/head.php"); ?>
+    <!-- main area part start -->
+    <main>
+
+      <section class="content-box-area mt-4">
+        <div class="container">
+          <div class="row g-4">
+            <?php include("dependable/profile.php"); ?>
+            <div class="col-xl-8">
+              <div class="card content-box-card">
+                <div class="card-body">
+                  <div class="top-info">
+                    <div class="text">
+                      <h1 class="main-title">Hi, This Is <span>Abdi-Moha</span> 👋</h1>
+                      <p>A Passionate <b>Flutter & Dart Developer</b> 🖥️, <b>Kotlin, Java & Python</b> having over <b> 5
+                        years</b> of Experience in and around Uganda</p>
+                    </div>
+                    <div class="available-btn">
+                      <span><i class="fas fa-circle"></i> Available For Hire</span>
+                    </div>
+                  </div>
+                  <div class="counter-area">
+                    <div class="counter">
+                      <div class="counter-item">
+                        <h3 class="number">7+</h3>
+                        <p class="subtitle">Year of Experience</p>
+                      </div>
+                      <div class="counter-item">
+                        <h3 class="number">36+</h3>
+                        <p class="subtitle">Project Completed
+                        </p>
+                      </div>
+                      <div class="counter-item">
+                        <h3 class="number">24+</h3>
+                        <p class="subtitle">Happy Client
+                        </p>
+                      </div>
+                    </div>
+                    <div class="circle-area">
+                      <div class="circle-text">
+                        <img class="circle-image" src="assets/img/about-us/circle-text.svg" alt="circle-text">
+                        <img class="circle-image circle-image-light d-none"
+                          src="assets/img/about-us/circle-text-light.svg" alt="circle-text">
+                        <span class="arrow-down">
+                          <svg width="40" height="40" viewBox="0 0 40 40" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20 5V35" stroke="white" stroke-width="2" stroke-linecap="round"
+                              stroke-linejoin="round" />
+                            <path d="M15 30L20 35L25 30" stroke="white" stroke-width="2" stroke-linecap="round"
+                              stroke-linejoin="round" />
+                          </svg>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="working-with-area">
+                    <h2 class="main-common-title">Working With 8+ Brands ✨
+                    </h2>
+                    <div class="working-with-main">
+                      <div class="items">
+                        <img src="assets/img/icons/figma.svg" alt="notion">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/python.svg" alt="webflow">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/flutter.svg" alt="mico">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/kotlin.svg" alt="framer">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/dart.svg" alt="zeplin">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/java.svg" alt="figma">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/notion.svg" alt="notion">
+                      </div>
+                      <div class="items">
+                        <img src="assets/img/icons/androidstudion.svg" alt="androidstudion">
+                      </div>
+                     
+                    </div>
+                  </div>
+                  <div class="work-together-slider">
+                    <div class="slider-main d-flex gap-4 align-items-center">
+                      <div class="slider-item">
+                        <a href="contact_me.php">Let's 👋 Work Together</a>
+                        <a href="contact_me.php">Let's 👋 Work Together</a>
+                      </div>
+                      <div class="slider-item">
+                        <a href="contact_me.php">Let's 👋 Work Together</a>
+                        <a href="contact_me.php">Let's 👋 Work Together</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- background shape area start -->
+      <div class="background-shapes">
+        <div class="shape-1 common-shape">
+          <img src="assets/img/bg/banner-shape-1.png" alt="banner-shape-1">
+        </div>
+        <div class="shape-2 common-shape">
+          <img src="assets/img/bg/banner-shape-1.png" alt="banner-shape-1">
+        </div>
+        <div class="threed-shape-1 move-with-cursor" data-value="1">
+          <img src="assets/img/bg/object-3d-1.png" alt="object-3d-1">
+        </div>
+        <div class="threed-shape-2 move-with-cursor" data-value="1">
+          <img src="assets/img/bg/object-3d-2.png" alt="object-3d-2">
+        </div>
+      </div>
+      <!-- background shape area end -->
+    </main>
+    <!-- main area part end -->
+<?php include("dependable/footer.php"); ?>
---
+++ moha/index.php
@@ -0,0 +1,380 @@
+  <?php include("dependable/head.php"); ?>
+    <!-- main area part start -->
+    <main>
+      <section class="home-area">
+        <div class="container">
+          <div class="row g-4">
+            <?php include("dependable/profile.php"); ?>
+            <div class="col-xl-4">
+              <div class="row g-4">
+                <div class="col-lg-12">
+                  <div class="card">
+                    <div class="card-body work-experiance-card">
+                      <h3 class="card-title">Work Experience</h3>
+                      <div class="work-experiance-main">
+                        <ul class="work-experiance-slider list-unstyled">
+                          <li>
+                            <div class="date">
+                              <p>2018 todate</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/flutter.svg" alt="google">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">92 Cafe App
+                                </h4>
+                                <h6 class="subtitle">Senior Product Designer
+  
+                                </h6>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="date">
+                              <p>2021-2024</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/gitAward.png" alt="adobe">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">GitHub Trophy</h4>
+                                <h6 class="subtitle">UI UX Designer</h6>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="date">
+                              <p>2016-2022</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/dart.svg" alt="meta">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Task manager App
+                                </h4>
+                                <h6 class="subtitle">Product Designer
+                                </h6>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="date">
+                              <p>2020-2021</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/python.svg" alt="adobe">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Ticketing App</h4>
+                                <h6 class="subtitle">Senior UI UX Designer</h6>
+                              </div>
+                            </div>
+                          </li>
+                        </ul>
+                        <ul class="work-experiance-slider list-unstyled">
+                          <li>
+                            <div class="date">
+                              <p>2018 todate</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/flutter.svg" alt="google">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">92 Cafe App
+                                </h4>
+                                <h6 class="subtitle">Senior Product Designer
+  
+                                </h6>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="date">
+                              <p>2021-2024</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/gitAward.png" alt="adobe">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">GitHub Trophy</h4>
+                                <h6 class="subtitle">UI UX Designer</h6>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="date">
+                              <p>2016-2022</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/dart.svg" alt="meta">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Task manager App
+                                </h4>
+                                <h6 class="subtitle">Product Designer
+                                </h6>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="date">
+                              <p>2020-2021</p>
+                            </div>
+                            <div class="info">
+                              <div class="icon">
+                                <img src="assets/img/icons/python.svg" alt="adobe">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Ticketing App</h4>
+                                <h6 class="subtitle">Senior UI UX Designer</h6>
+                              </div>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="card expertise-card">
+                    <div class="card-body">
+                      <h3 class="card-title">My Expert Area
+                      </h3>
+                      <div class="expertise-main mt-24">
+                        <div class="row g-3">
+                          <div class="col-xl-4 col-md-4 col-sm-6 col-6">
+                            <div class="expertise-item">
+                              <div class="image text-center">
+                                <img src="assets/img/icons/figma.svg" alt="figma">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Figma</h4>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-xl-4 col-md-4 col-sm-6 col-6">
+                            <div class="expertise-item">
+                              <div class="image text-center">
+                                <img src="assets/img/icons/python.svg" alt="notion">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Python</h4>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-xl-4 col-md-4 col-sm-6 col-6">
+                            <div class="expertise-item">
+                              <div class="image text-center">
+                                <img src="assets/img/icons/kotlin.svg" alt="micro">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Kotlin</h4>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-xl-4 col-md-4 col-sm-6 col-6">
+                            <div class="expertise-item">
+                              <div class="image text-center">
+                                <img src="assets/img/icons/flutter.svg" alt="framer">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Flutter</h4>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-xl-4 col-md-4 col-sm-6 col-6">
+                            <div class="expertise-item">
+                              <div class="image text-center">
+                                <img src="assets/img/icons/dart.svg" alt="webflow">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Dart</h4>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-xl-4 col-md-4 col-sm-6 col-6">
+                            <div class="expertise-item">
+                              <div class="image text-center">
+                                <img src="assets/img/icons/java.svg" alt="zeplin">
+                              </div>
+                              <div class="text">
+                                <h4 class="title">Java</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4">
+              <div class="card card-projects">
+                <div class="card-body">
+  
+                  <h3 class="card-title">Recent Projects <a class="link-btn" href="portfolio.html">All Projects
+                      <svg class="icon" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.16699 10H15.8337" stroke="#4770FF" stroke-width="1.5" stroke-linecap="round"
+                          stroke-linejoin="round" />
+                        <path d="M10.833 15L15.833 10" stroke="#4770FF" stroke-width="1.5" stroke-linecap="round"
+                          stroke-linejoin="round" />
+                        <path d="M10.833 5L15.833 10" stroke="#4770FF" stroke-width="1.5" stroke-linecap="round"
+                          stroke-linejoin="round" />
+                      </svg>
+  
+                    </a></h3>
+                  <div class="projects-main mt-24">
+                    <div class="row g-4 parent-container">
+                      <div class="col-lg-12">
+                        <div class="project-item">
+                          <div class="image">
+                            <img src="assets/img/projects/project-1.png" alt="project-1" class="img-fluid w-100">
+                            <a href="assets/img/projects/project-1.png"
+                              class="gallery-popup full-image-preview parent-container">
+                              <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none"
+                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                                <path d="M10 4.167v11.666M4.167 10h11.666"></path>
+                              </svg>
+                            </a>
+                            <div class="info">
+                              <span class="category">Mobile App UI/Ux</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-lg-12">
+                        <div class="project-item">
+                          <div class="image">
+                            <img src="assets/img/projects/project-2.png" alt="project-2" class="img-fluid w-100">
+                            <a href="assets/img/projects/project-2.png"
+                              class="gallery-popup full-image-preview parent-container">
+                              <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none"
+                                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                                <path d="M10 4.167v11.666M4.167 10h11.666"></path>
+                              </svg>
+                            </a>
+                            <div class="info">
+                              <span class="category">Mobile App UI/Ux</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="services-area mt-24">
+            <div class="row g-4">
+              <div class="col-xl-8">
+                <div class="card services-card">
+                  <div class="card-body">
+                    <h3 class="card-title">Services I Offer
+                      </h3>
+                    <div class="services-main mt-24">
+                      <div class="row g-4">
+                        <div class="col-md-3 col-sm-6 col-6">
+                          <div class="services-item text-center">
+                            <div class="image">
+                              <img src="assets/img/icons/ui-ux.svg" alt="ui-ux">
+                            </div>
+                            <div class="text">
+                              <h3 class="title">UI UX Design</h3>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-6">
+                          <div class="services-item text-center">
+                            <div class="image">
+                              <img src="assets/img/icons/app.svg" alt="app">
+                            </div>
+                            <div class="text">
+                              <h3 class="title">Mobile App</h3>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-6">
+                          <div class="services-item text-center">
+                            <div class="image">
+                              <img src="assets/img/icons/prd-design.svg" alt="prd-design">
+                            </div>
+                            <div class="text">
+                              <h3 class="title">Product Design</h3>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-6">
+                          <div class="services-item text-center">
+                            <div class="image">
+                              <img src="assets/img/icons/branding.svg" alt="branding">
+                            </div>
+                            <div class="text">
+                              <h3 class="title">Branding</h3>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-4">
+                <div class="card lets-talk-together-card">
+                  <div class="card-body">
+                    <div class="scrolling-info">
+                      <div class="slider-item">
+                        <p>
+                          Available For Hire 🚀 Crafting Digital Experiences 🎨 Available For Hire 🚀 Crafting Digital
+                          Experiences 🎨
+                        </p>
+                      </div>
+                    </div>
+                    <h3 class="card-title">Let's👋
+                      <span class="d-block">Work Together</span>
+                    </h3>
+                    <a class="link-btn" href="contact_me.php"> Let's Talk
+                      <svg class="icon" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17.5 11.6665V6.6665H12.5" stroke-width="1.5" stroke-linecap="round"
+                          stroke-linejoin="round" />
+                        <path d="M17.5 6.6665L10 14.1665L2.5 6.6665" stroke-width="1.5" stroke-linecap="round"
+                          stroke-linejoin="round" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- background shape area start -->
+      <div class="background-shapes">
+        <div class="shape-1 common-shape">
+          <img src="assets/img/bg/banner-shape-1.png" alt="banner-shape-1">
+        </div>
+        <div class="shape-2 common-shape">
+          <img src="assets/img/bg/banner-shape-1.png" alt="banner-shape-1">
+        </div>
+        <div class="threed-shape-1 move-with-cursor" data-value="1">
+          <img src="assets/img/bg/object-3d-1.png" alt="object-3d-1">
+        </div>
+        <div class="threed-shape-2 move-with-cursor" data-value="1">
+          <img src="assets/img/bg/object-3d-2.png" alt="object-3d-2">
+        </div>
+      </div>
+      <!-- background shape area end -->
+    </main>
+    <!-- main area part end -->
+  
+<?php include("dependable/footer.php"); ?>
